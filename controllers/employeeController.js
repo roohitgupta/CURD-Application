6182241bc2f0fdae836f751baae0edb628f23701
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-06-03 03:42:07 +0530

    added insert Employee UI page and set handlebars

---
+++ controllers/employeeController.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.render('employee/addOrEdit',{
+        viewTitle : "Insert Employee"
+    });
+});
+
+module.exports = router;
